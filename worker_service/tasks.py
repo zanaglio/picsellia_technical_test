5a754e648fd506652aa0653896f449debe583f19
Alexis Schutzger <alexis@mozartthemacbook.home>
2023-02-17 15:56:37 +0100

    feat: Added a mocked version of the prediction model

--- worker_service/tasks.py
+++ worker_service/tasks.py
@@ -1,4 +1,7 @@
 from celery_app import celery
+from model.model import predict
+from utils.image import base64_to_array
+from utils.outline import get_objects_vertices
 
 
 @celery.task(name="outline.objects.tasks.process", bind=True)
@@ -6,5 +9,11 @@ def process_image(self, image_as_base64: str) -> list:
     def update_task_state(state: str, step: int, total: int):
         self.update_state(state=state, meta={"step": step, "total": total})
 
-    update_task_state("Retrieving image", 1, 2)
-    return [len(image_as_base64)]
+    update_task_state("Performing the prediction", 1, 2)
+    image_data = base64_to_array(image_as_base64)
+    prediction = predict(image_data)
+
+    update_task_state("Computing the objects' vertices", 2, 2)
+    object_vertices = get_objects_vertices(prediction)
+
+    return object_vertices
---
+++ worker_service/utils/outline.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def get_objects_vertices(image_data: np.array) -> list:
+    return []
